54f5e01d8ebe1f862262932406d27eb314426ade
Sun Podder <contact.sunpodder09@gmail.com>
2025-07-06 12:20:12 +0600

    refactor: migrate validation from zod to valibot and update package dependencies
    - Removed zod schema definitions and replaced them with valibot equivalents in zod_schema.ts.
    - Updated user registration, login, and profile validation schemas to use valibot's validation methods.
    - Deleted bun.lockb file.
    - Added valibot as a dependency and updated devDependencies in package.json.

--- common/zod_schema.ts
+++ common/zod_schema.ts
@@ -1,30 +1,56 @@
-import { z } from "zod";
+import * as v from 'valibot';
 
-export const zRegisterUser = z.object({
-	name: z.object({
-		first: z.string().min(2, "Must contain atleast 2 characters").max(7),
-		last: z.string().min(2, "Must contain atleast 2 characters").max(7),
+export const vRegisterUser = v.object({
+	name: v.object({
+		first: v.pipe(
+			v.string(), 
+			v.minLength(2, "Must contain atleast 2 characters"), 
+			v.maxLength(7)
+		),
+		last: v.pipe(
+			v.string(), 
+			v.minLength(2, "Must contain atleast 2 characters"), 
+			v.maxLength(7)
+		),
 	}),
-	username: z.string().min(3, "Must contain atleast 3 characters").max(10),
-	email: z.string().email(),
-	password: z.string().min(8).max(20),
+	username: v.pipe(
+		v.string(), 
+		v.minLength(3, "Must contain atleast 3 characters"), 
+		v.maxLength(10)
+	),
+	email: v.pipe(v.string(), v.email()),
+	password: v.pipe(v.string(), v.minLength(8), v.maxLength(20)),
 });
 
-export const zLoginUser = z.object({
-	email: z.string().email(),
-	password: z.string().min(8).max(20),
+export const vLoginUser = v.object({
+	email: v.pipe(v.string(), v.email()),
+	password: v.pipe(v.string(), v.minLength(8), v.maxLength(20)),
 });
 
 // const ACCEPTED_FILE_TYPES = ["image/jpeg", "image/jpg", "image/png"];
 
-export const zProfile = z.object({
-	name: z.object({
-		first: z.string().min(2, "Must contain atleast 2 characters").max(7),
-		last: z.string().min(2, "Must contain atleast 2 characters").max(7),
+export const vProfile = v.object({
+	name: v.object({
+		first: v.pipe(
+			v.string(), 
+			v.minLength(2, "Must contain atleast 2 characters"), 
+			v.maxLength(7)
+		),
+		last: v.pipe(
+			v.string(), 
+			v.minLength(2, "Must contain atleast 2 characters"), 
+			v.maxLength(7)
+		),
 	}),
-	username: z.string().min(3, "Must contain atleast 3 characters").max(10),
-	email: z.string().email(),
-	avatar: z
-		.any()
-		.optional()
+	username: v.pipe(
+		v.string(), 
+		v.minLength(3, "Must contain atleast 3 characters"), 
+		v.maxLength(10)
+	),
+	email: v.pipe(v.string(), v.email()),
+	avatar: v.optional(v.any()),
 });
+
+export type RegisterUserSchema = v.InferOutput<typeof vRegisterUser>;
+export type LoginUserSchema = v.InferOutput<typeof vLoginUser>;
+export type ProfileSchema = v.InferOutput<typeof vProfile>;
