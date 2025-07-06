75e69ac15dc2998ce450e3bac87bb17752a5afbf
SunPodder <SunPodder@users.noreply.github.com>
2025-07-06 06:29:14 +0000

    Deploying to gh-pages from @ SunPodder/chat-app@5a38e50c613b5ac093426f7e734fe94db0744675 🚀

---
+++ assets/Topbar-1I20cj9K.js
@@ -0,0 +1,6 @@
+import{j as e}from"./index-BhQeG5K_.js";import{c as a}from"./createLucideIcon-8aQ_-RDO.js";/**
+ * @license lucide-react v0.446.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const s=a("MessageSquare",[["path",{d:"M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z",key:"1lielz"}]]);function l(){return e.jsx("header",{className:"hidden top-0 z-10 md:flex h-[57px] items-center gap-1 border-b bg-background px-4 w-full",children:e.jsxs("h1",{className:"text-xl font-semibold flex items-center",children:[e.jsx(s,{className:"size-5"}),"RChat"]})})}export{l as default};
